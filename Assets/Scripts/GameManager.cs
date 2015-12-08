2b7a40b6446c7eb99462f9cf38a4240b60e75641
Brent <brent.spector@yahoo.com>
2015-12-08 15:20:06 -0800

    Initial Commit of Project
    This commit establishes the majority of pokemon sprites to be used, the
    title card for the game, and the files to ignore in this repository.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	//Singleton Variable
+	public static GameManager instance = null;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		//If an instance doesn't exist, set it to this
+		if (instance == null) 
+		{
+			instance = this;
+		} //end if
+        //If an instance exists that isn't this, destroy this
+        else if (instance != this) 
+		{
+			Destroy (gameObject);
+		} //end else if	
+
+		//Keep GameManager from destruction OnLoad
+		DontDestroyOnLoad (instance);
+	} //end Awake
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	} //end Update
+} //end GameManager class
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour 
+{
+    //Holds the prefab of the GameManager
+    public GameManager gameManager;
+
+	// Use this for initialization
+	void Awake () 
+    {  
+        //If a GameManager doesn't exist, make one
+        if(GameManager.instance == null)
+        {
+            Instantiate(gameManager);
+        } //end if	
+	} //end Awake
+} //end Loader class
